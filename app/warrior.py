770c80ae7716284d958347c7753b806fb44d5db2
Denis <denis.spb02@mail.ru>
2021-04-08 15:25:51 +0300

    Added architecture description and deleted unnecessary functions

--- app/warrior.py
+++ app/warrior.py
@@ -1,34 +1,14 @@
-from enum import Enum
-
-
-class WarriorType(Enum):
-    Warrior = 'Warrior'
-    Archer = 'Archer'
-    Infantryman = 'Infantryman'
-    Horseman = 'Horseman'
-    Swordsman = 'Swordsman'
-
-
 class Warrior:
-    type = WarriorType.Warrior
-
     def __init__(self):
         self.power = 0
         self.health = 0
         self.alive = True
 
-    def attack(self, other):
-        other.health -= self.power
-        if other.health <= 0:
-            other.alive = False
-
-    def warrior_status(self):
-        return 'type: {}\nhealth: {}\npower: {}\n'.format(self.type.value, self.health, self.power)
+    def warrior_status(self, warrior_type):
+        return 'type: {}\nhealth: {}\npower: {}\n'.format(warrior_type, self.health, self.power)
 
 
 class Archer(Warrior):
-    type = WarriorType.Archer
-
     def __init__(self):
         super().__init__()
         self.health = 42
@@ -37,8 +17,6 @@ class Archer(Warrior):
 
 
 class Infantryman(Warrior):
-    type = WarriorType.Infantryman
-
     def __init__(self):
         super().__init__()
         self.health = 38
@@ -47,8 +25,6 @@ class Infantryman(Warrior):
 
 
 class Horseman(Warrior):
-    type = WarriorType.Horseman
-
     def __init__(self):
         super().__init__()
         self.health = 30
@@ -57,8 +33,6 @@ class Horseman(Warrior):
 
 
 class Swordsman(Warrior):
-    type = WarriorType.Swordsman
-
     def __init__(self):
         super().__init__()
         self.health = 36
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from app import game
 
-
-def main():
-    new_game = game.Game()
-    new_game.play()
-
-
 if __name__ == '__main__':
-    main()
+    new_game = game.Game()
+    new_game.start_battle()
