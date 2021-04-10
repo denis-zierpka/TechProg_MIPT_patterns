8427f9dd034be76d0d60a21634812c18c257711f
Denis <denis.spb02@mail.ru>
2021-04-10 18:27:58 +0300

    Removed unnecessary code and files, changed FactoryRegister

--- app/game.py
+++ app/game.py
@@ -1,56 +1,6 @@
-from app.users import player1, player2
-from app.warrior_factory import factory_register
 from app.warrior_factory import WarriorFactory
-from app.warrior import Warrior
-
-
-def enter_number_mes():
-    print('Enter number from 1 to 4, standing for next warriors')
-    num = 1
-    for cls in Warrior.__subclasses__():
-        print(str(num) + ' ' + str(cls.__name__))
-        num += 1
-    print('-----------------')
-
-
-def print_start_info():
-    print('-----------------')
-    print('Player1 status:')
-    print(player1.player_status())
-    print('-----------------')
-    print('Player2 status:')
-    print(player2.player_status())
 
 
 class Game:
     def __init__(self):
-        self.winner = 0
         WarriorFactory()
-
-    def choose_warriors(self, player):
-        enter_number_mes()
-        for i in range(3):
-            while True:
-                try:
-                    a = int(input())
-                except Exception:
-                    print("Error!")
-                    continue
-
-                if a < 1 or a > 4:
-                    print("Error!")
-                    continue
-                player.add_warrior(factory_register.list[a - 1].create_warrior(Warrior))
-                break
-
-    def start_battle(self):
-        print('Player1:\nChoose 3 warriors you would like to see in your team')
-        self.choose_warriors(player1)
-        print('Player2:\nChoose 3 warriors you would like to see in your team')
-        self.choose_warriors(player2)
-
-        print_start_info()
-
-    def play(self):
-        # implement graphics
-        self.start_battle()
--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from app import game
 
 if __name__ == '__main__':
     new_game = game.Game()
-    new_game.start_battle()
