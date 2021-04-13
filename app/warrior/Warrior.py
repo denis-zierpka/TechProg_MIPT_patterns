c8b39790eb9780c875a473cad2e8216655eb1781
Denis <denis.spb02@mail.ru>
2021-04-14 01:44:07 +0300

    added status display

--- app/warrior/Warrior.py
+++ app/warrior/Warrior.py
@@ -3,3 +3,6 @@ class Warrior:
         self.power = 0
         self.health = 0
         self.alive = True
+
+    def warrior_status(self, warrior_type):
+        return 'type: {}\nhealth: {}\npower: {}\n'.format(warrior_type, self.health, self.power)
